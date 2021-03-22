7963a44bee963a030a0ebfa69b323634ac78b004
Maksym Ushakov <ushakov.m@hotmail.com>
2021-03-22 21:05:11 +0200

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  return members.reduce( (acc, el) => typeof(el) === 'string' ? acc.concat(el.trim().substring(0,1).toUpperCase()) : acc, [] ).sort().join('');
 };
