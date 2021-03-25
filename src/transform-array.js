02f68a071ac171675cc9c44131105fe40ad7925f
Maksim Ushakov <ushakov.m@hotmail.com>
2021-03-25 22:27:14 +0200

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error;
+  }
+  let discardNext = false;
+  let lastEmptyItem = -1;
+  return arr.reduce((acc, el, i, array) => {
+    if (discardNext) {
+      discardNext = false;
+      lastEmptyItem = i;
+      return acc;
+    }
+    if (el === '--discard-next') {
+      discardNext = true;
+      return acc;
+    }
+    if (el === '--discard-prev') {
+      if (acc.length > 0 && lastEmptyItem !== --i) {
+        acc.pop();
+      }
+      return acc;
+    }
+    if (el === '--double-next') {
+      if (++i < array.length) {
+        acc.push(array[i]);
+      }
+      return acc;
+    }
+    if (el === '--double-prev') {
+      if (i > 0 && lastEmptyItem !== --i) {
+        acc.push(array[i]);
+      }
+      return acc;
+    }
+    acc.push(el);
+    return acc;
+  }, [])
 };
